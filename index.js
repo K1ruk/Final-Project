31540a4fbee6b0bfec53d7abeafb51a423250bbd
K1ruk <148961896+K1ruk@users.noreply.github.com>
2023-11-12 02:09:43 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function openMenu() {
+    document.getElementById("SideBar").style.width = "90vw";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeMenu() {
+    document.getElementById("SideBar").style.width = "0";
+  }
