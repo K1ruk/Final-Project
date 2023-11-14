9c5036d4040b87faef4e34fe244c6555321b135e
K1ruk <148961896+K1ruk@users.noreply.github.com>
2023-11-14 06:38:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 function openMenu() {
-    document.getElementById("SideBar").style.width = "90vw";
+    var y = document.getElementById("sideMenu");
+
+    if (y.style.display === "none")
+    {
+      y.style.display = "block";
+    } 
+    
+    else {
+      y.style.display = "none";
+    }
   }
   
   /* Set the width of the sidebar to 0 (hide it) */
   function closeMenu() {
     document.getElementById("SideBar").style.width = "0";
   }
+
+function openSearch()
+{
+  var x = document.getElementById("searchInput");
+
+  if (x.style.display === "none")
+  {
+    x.style.display = "inline";
+    x.style.transition = "0.4s ease-in-out";
+  } 
+  
+  else {
+    x.style.display = "none";
+  }
+}
