a881355174530850026bae2dac85a308582a7c32
K1ruk <148961896+K1ruk@users.noreply.github.com>
2023-12-04 08:56:00 +0100

    Add files via upload

--- Final Project/Index/index.js
+++ Final Project/Index/index.js
@@ -28,28 +28,100 @@ function openSearch()
     x.style.display = "none";
   }
 }
+function calculateSubtotal() {
+  let storedData = JSON.parse(localStorage.getItem('data')) || [];
+  let subtotal = 0;
+
+  storedData.forEach(item => {
+    subtotal += item.total || 0;
+  });
+
+  return subtotal;
+}
+
+function updateCart() {
+  // Calculate and display subtotal
+  const subtotal = calculateSubtotal();
+  console.log("Subtotal: ", subtotal);
 
 
-function add(event){
+  //const subtotalElement = document.getElementById('subtotal');
+  //subtotalElement.innerText = `Subtotal: ${subtotal}`;
+}
+function updateCartIndicator() {
+  let storedData = JSON.parse(localStorage.getItem('data')) || [];
+  const cartIndicator = document.getElementById('cart-indicator');
+
+  // Calculate the total number of items in the cart
+  const totalItems = storedData.reduce((total, item) => total + item.amount, 0);
+
+  // Update the cart indicator
+  cartIndicator.innerText = totalItems.toString();
+}
+
+function add(event) {
   var button = event.target;
   var shopItem = button.parentElement;
-  var title = shopItem.getElementsByClassName("card-title")[0].innerText;
-  localStorage.setItem("title", title);
+  const title = shopItem.querySelector(".card-title h1").innerText;
+  const price = parseFloat(shopItem.querySelector(".price").innerText.replace(':-', '')); // Parse price as float
+  const amount =+ shopItem.querySelector(".count").value; // Convert to number
 
-  console.log(title);
+  const data = {
+    title: title,
+    price: price,
+    amount: amount,
+    total: price * amount // Calculate total for the current item
+  };
 
+  let storedData = JSON.parse(localStorage.getItem('data')) || [];
+
+  const existingItemIndex = storedData.findIndex(item => item.title === title);
+
+  if (existingItemIndex !== -1) {
+    storedData[existingItemIndex].amount += amount;
+    storedData[existingItemIndex].total = storedData[existingItemIndex].price * storedData[existingItemIndex].amount;
+  } else {
+    storedData.push(data);
+  }
+
+  localStorage.setItem('data', JSON.stringify(storedData));
+
+  console.log("Stored data: ", storedData);
+
+  updateCartIndicator(); // Update the cart indicator after adding an item
+  updateCart(); // Update the cart after adding an item
 }
 
-function cartClicked(){
-
-
-  window.location.href = "/Cart/card.html";
-  var titles = document.getElementsByClassName("cart-title"); 
-  var mytitle = localStorage.getItem("title");
-  document.getElementById("newTitle").textContent = mytitle; 
-  for(var i = 0, i = title.Length ){};
-  console.log(mytitle);
+document.addEventListener('DOMContentLoaded', function () {
   
+  // Retrieve data from localStorage and making it into an array.
+  const storedData = JSON.parse(localStorage.getItem('data')) || [];
 
+  // Get the container where you want to display the data
+  const cartContent = document.getElementById('cart-content');
+
+  // Check if there is data to display
+  if (storedData.length > 0) {
+    // Iterate through the stored data and create HTML elements
+    storedData.forEach(item => {
+      const itemElement = document.createElement('div');
+      itemElement.classList.add("grid");
+      itemElement.setAttribute("id","product");
+      itemElement.innerHTML = `
+        <h1>${item.title}</h1>
+        <p class="price">${item.price} kr</p>
+        <p class="quantity">${item.amount}</p>
+        <p class="total">${item.total} kr</p>
+      `;
+      cartContent.appendChild(itemElement);
+    });
+  } else {
+    // Display a message if there is no data
+    cartContent.innerHTML = '<h1>Your cart is empty.</h1>';
+  }
+});
+
+function empty(){
+  localStorage.clear();
+  location.reload();
 }
-
