cb6f5f3e1cb8daf4fcaa172c31dbeec7188c6974
K1ruk <148961896+K1ruk@users.noreply.github.com>
2023-12-06 10:34:06 +0100

    Add files via upload

--- Final Project/Index/index.js
+++ Final Project/Index/index.js
@@ -28,18 +28,19 @@ function openSearch()
     x.style.display = "none";
   }
 }
-function calculateSubtotal() {
+
+function calculateSubtotal()
+{
   let storedData = JSON.parse(localStorage.getItem('data')) || [];
   let subtotal = 0;
 
-  storedData.forEach(item => {
-    subtotal += item.total || 0;
-  });
+  storedData.forEach(item => { subtotal += item.total || 0;}); //0 
 
   return subtotal;
 }
 
-function updateCart() {
+function updateCart() 
+{
   // Calculate and display subtotal
   const subtotal = calculateSubtotal();
   console.log("Subtotal: ", subtotal);
@@ -48,13 +49,14 @@ function updateCart() {
   //const subtotalElement = document.getElementById('subtotal');
   //subtotalElement.innerText = `Subtotal: ${subtotal}`;
 }
-function updateCartIndicator() {
+
+function updateCartIndicator() 
+{
   let storedData = JSON.parse(localStorage.getItem('data')) || [];
   const cartIndicator = document.getElementById('cart-indicator');
 
   // Calculate the total number of items in the cart
   const totalItems = storedData.reduce((total, item) => total + item.amount, 0);
-
   // Update the cart indicator
   cartIndicator.innerText = totalItems.toString();
 }
@@ -66,7 +68,8 @@ function add(event) {
   const price = parseFloat(shopItem.querySelector(".price").innerText.replace(':-', '')); // Parse price as float
   const amount =+ shopItem.querySelector(".count").value; // Convert to number
 
-  const data = {
+  const data = 
+  {
     title: title,
     price: price,
     amount: amount,
@@ -75,17 +78,24 @@ function add(event) {
 
   let storedData = JSON.parse(localStorage.getItem('data')) || [];
 
-  const existingItemIndex = storedData.findIndex(item => item.title === title);
 
-  if (existingItemIndex !== -1) {
-    storedData[existingItemIndex].amount += amount;
-    storedData[existingItemIndex].total = storedData[existingItemIndex].price * storedData[existingItemIndex].amount;
-  } else {
+  // findIndex method is used here to get an index of an item in the array and checks if obtained "title" variable is in the existing array (item => item.title).
+  //It should return -1 if the title is not in the array or it should return (n)th number of index if the obtained "title" is in the array. 
+  const ItemIndex = storedData.findIndex(item => item.title === title); //title is used here for searching an item in the array based on the title.
+
+  // If itemIndex is not equal -1, meaning the item is already in the array, it should change the amount and total price of the item.
+  if(ItemIndex !== -1)
+  {
+    storedData[ItemIndex].amount += amount;
+    storedData[ItemIndex].total = storedData[ItemIndex].price * storedData[ItemIndex].amount;
+  } 
+  //or else, meaning the item is not in the array, the non-existing item will push its data into the array
+  else
+  {
     storedData.push(data);
   }
 
   localStorage.setItem('data', JSON.stringify(storedData));
-
   console.log("Stored data: ", storedData);
 
   updateCartIndicator(); // Update the cart indicator after adding an item
@@ -101,7 +111,8 @@ document.addEventListener('DOMContentLoaded', function () {
   const cartContent = document.getElementById('cart-content');
 
   // Check if there is data to display
-  if (storedData.length > 0) {
+  if (storedData.length > 0) 
+  {
     // Iterate through the stored data and create HTML elements
     storedData.forEach(item => {
       const itemElement = document.createElement('div');
@@ -115,7 +126,10 @@ document.addEventListener('DOMContentLoaded', function () {
       `;
       cartContent.appendChild(itemElement);
     });
-  } else {
+  } 
+
+  else 
+  {
     // Display a message if there is no data
     cartContent.innerHTML = '<h1>Your cart is empty.</h1>';
   }
@@ -124,4 +138,8 @@ document.addEventListener('DOMContentLoaded', function () {
 function empty(){
   localStorage.clear();
   location.reload();
+}
+
+function checkout(){
+  window.location.href = "/Checkout page/checkout.html";
 }
