2ee35a8d868ab678be57d35c58e116eae6713d7e
K1ruk <148961896+K1ruk@users.noreply.github.com>
2023-12-08 02:09:23 +0100

    Add files via upload

--- Final Project/Index/index.js
+++ Final Project/Index/index.js
@@ -3,9 +3,8 @@ var addButton = document.getElementById('menu');
 
 // Add an event listener to the menu button
 addButton.addEventListener('click', function() {
-
   // Add both nav-menu-mobile and nav-menu-mobile-active to the existing classes when the button is clicked
-  myElement.classList.add('nav-menu-mobile-active');
+  myElement.classList.add('nav-menu-mobile-active'); 
 });
 
 
@@ -28,79 +27,83 @@ function openSearch()
     x.style.display = "none";
   }
 }
-function calculateSubtotal() {
+
+function calculateSubtotal()
+{
   let storedData = JSON.parse(localStorage.getItem('data')) || [];
-  let subtotal = 0;
+  let total = 0;
 
-  storedData.forEach(item => {
-    subtotal += item.total || 0;
-  });
+  storedData.forEach(item => { total += item.subtotal || 0;}); //0 
 
-  return subtotal;
+  return total;
 }
 
-function updateCart() {
+function updateCart() 
+{
   // Calculate and display subtotal
-  const subtotal = calculateSubtotal();
-  console.log("Subtotal: ", subtotal);
+  const total = calculateSubtotal();
+  console.log("Total: ", total);
 
 
   //const subtotalElement = document.getElementById('subtotal');
   //subtotalElement.innerText = `Subtotal: ${subtotal}`;
 }
-function updateCartIndicator() {
+
+function updateCartIndicator() 
+{
   let storedData = JSON.parse(localStorage.getItem('data')) || [];
   const cartIndicator = document.getElementById('cart-indicator');
 
   // Calculate the total number of items in the cart
-  const totalItems = storedData.reduce((total, item) => total + item.amount, 0);
-
+  const totalItems = storedData.reduce((subtotal, item) => subtotal + item.amount, 0);
   // Update the cart indicator
-  cartIndicator.innerText = totalItems.toString();  
+  cartIndicator.innerText = totalItems.toString();
 }
 
-function add(event) 
-{
+function add(event) {
   var button = event.target;
   var shopItem = button.parentElement;
   const title = shopItem.querySelector(".card-title h1").innerText;
   const price = parseFloat(shopItem.querySelector(".price").innerText.replace(':-', '')); // Parse price as float
   const amount =+ shopItem.querySelector(".count").value; // Convert to number
 
-  //Obj
-  const data = {
+  const data = 
+  {
     title: title,
     price: price,
     amount: amount,
-    total: price * amount // Calculate total for the current item
+    subtotal: price * amount // Calculate total for the current item
   };
 
   let storedData = JSON.parse(localStorage.getItem('data')) || [];
 
-  //findIndex method returns -1 if no match is found.
-  const existingItemIndex = storedData.findIndex(item => item.title === title);
 
-  //if existingItemIndex returns no other than -1 meaning this item is already in the array and changes only the amount and total price.
-  if (existingItemIndex !== -1) 
+  // findIndex method is used here to get an index of an item in the array and checks if obtained "title" variable is in the existing array (item => item.title).
+  //It should return -1 if the title is not in the array or it should return (n)th number of index if the obtained "title" is in the array. 
+  const ItemIndex = storedData.findIndex(item => item.title === title); //title is used here for searching an item in the array based on the title.
+
+  // If itemIndex is not equal -1, meaning the item is already in the array, it should change the amount and total price of the item.
+  if(ItemIndex !== -1)
   {
-    storedData[existingItemIndex].amount += amount;
-    storedData[existingItemIndex].total = storedData[existingItemIndex].price * storedData[existingItemIndex].amount;
+    storedData[ItemIndex].amount += amount;
+    storedData[ItemIndex].subtotal = storedData[ItemIndex].price * storedData[ItemIndex].amount;
   } 
-  //if the itemIndex is -1 then the item is not in the array and pushes to an array.
-  else 
+  //or else, meaning the item is not in the array, the non-existing item will push its data into the array
+  else
   {
     storedData.push(data);
   }
 
   localStorage.setItem('data', JSON.stringify(storedData));
   console.log("Stored data: ", storedData);
+
   updateCartIndicator(); // Update the cart indicator after adding an item
   updateCart(); // Update the cart after adding an item
 }
 
-
 document.addEventListener('DOMContentLoaded', function () {
   
+  updateCartIndicator();
   // Retrieve data from localStorage and making it into an array.
   const storedData = JSON.parse(localStorage.getItem('data')) || [];
 
@@ -116,21 +119,38 @@ document.addEventListener('DOMContentLoaded', function () {
       itemElement.classList.add("grid");
       itemElement.setAttribute("id","product");
       itemElement.innerHTML = `
-        <h1>${item.title}</h1>
+        <h1 class="head">${item.title}</h1>
         <p class="price">${item.price} kr</p>
         <p class="quantity">${item.amount}</p>
-        <p class="total">${item.total} kr</p>
+        <p class="subtotal">${item.subtotal} kr</p>
       `;
       cartContent.appendChild(itemElement);
     });
   } 
-  else {
-    // Display a message if there is no data
-    cartContent.innerHTML = '<h1>Your cart is empty.</h1>';
+
+  else 
+  {
+    // Display a message if there is no data'
+    cartContent.innerHTML = '<h1>Your cart is empty</h1>';
   }
+  
 });
 
 function empty(){
   localStorage.clear();
   location.reload();
+}
+
+function checkout(){
+  const storedData = JSON.parse(localStorage.getItem('data')) || [];
+  const danger = document.getElementById('danger');
+
+  if(storedData.length > 0)
+  {
+    window.location.href = "/Checkout page/checkout.html";
+  }
+  else{
+    danger.style.display = "block";
+  }
+  
 }
